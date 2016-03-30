82a23e7573c58fdc00836cc0ba19183cd7db1235
Rasmag <magsteral@gmail.com>
2016-03-30 17:42:38 +0200

    Lancement

---
+++ NewNews/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Mef;
+
+namespace NewNews
+{
+	public class Bootstrapper:MefBootstrapper
+	{
+		protected override DependencyObject CreateShell()
+		{
+			return this.Container.GetExportedValue<MainWindow>();
+		}
+
+		protected override void InitializeShell()
+		{
+			base.InitializeShell();
+			Application.Current.MainWindow = (Window)this.Shell;
+			Application.Current.MainWindow.Show();
+		}
+
+		protected override void ConfigureAggregateCatalog()
+		{
+			this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(this.GetType().Assembly));
+		}
+	}
+}
---
+++ NewNews/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
+
+namespace NewNews
+{
+	public class MainWindowViewModel
+	{
+		public MainWindowViewModel()
+		{
+			var node = new Uri("http://localhost:9200");
+			var settings = new ConnectionSettings(node);
+			settings.DefaultIndex("node-0");
+			var client = new ElasticClient(settings);
+		}
+	}
+}
